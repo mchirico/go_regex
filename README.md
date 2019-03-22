83ca9e9537a237f7e702fed12c7d92cfe19b1457
Mike Chirico <mchirico@gmail.com>
2019-03-22 06:21:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Build Status](https://travis-ci.org/mchirico/go_regx.svg?branch=master)](https://travis-ci.org/mchirico/go_regx)
+# go_regx
---
+++ cmd/main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	SetupFunction()
+	retCode := m.Run()
+	TeardownFunction()
+	os.Exit(retCode)
+}
+
+func SetupFunction() {
+	flag.Parse()
+}
+
+func TeardownFunction() {
+
+}
+
+func TestReadFixtures(t *testing.T) {
+
+	file := "../fixtures/fixture_a.txt"
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		t.FailNow()
+	}
+	if len(data) > 200 {
+		t.Logf("Success!\n")
+	} else {
+		t.Logf("Could not read file")
+	}
+}
